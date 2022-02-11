d909d020f5362261a4b14e8c043a05d9a1e915d7
Jessy <jessy.kennel11@gmail.com>
2022-02-11 14:51:02 +0100

    debut product

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,19 @@
 // Récupération des produits de l'API //
 
-async function getarticles() {
-  var articlesCatch = await fetch("http://localhost:3000/api/products");
+var str = window.location.href;
+var url = new URL(str);
+
+var id = url.searchParams.get("id");
+console.log(id);
+
+async function getarticle() {
+  var articlesCatch = await fetch("http://localhost:3000/api/products/" + id);
   return await articlesCatch.json();
 }
+getarticle().then((produit) => {
+  console.log(produit);
+
+  lesitems.innerHTML = `
+  
+  `;
+});
